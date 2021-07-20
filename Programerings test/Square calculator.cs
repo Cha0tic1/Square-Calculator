3e46371dd17f6b80d46b36e3fe19366a9e1f04e0
Cha0tic1 <87732058+Cha0tic1@users.noreply.github.com>
2021-07-21 00:21:37 +0200

    Add files via upload

---
+++ Programerings test/Square calculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Programerings_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+          int side1;
+          int side2;
+          int x;
+          int result;
+          int result2;
+
+          Console.Write("Please enter a side length(cm): ");
+          side1 = Convert.ToInt32(Console.ReadLine());
+          Console.Write("\nPlease enter the second side(cm): ");
+          side2 = Convert.ToInt32(Console.ReadLine());
+
+          Console.WriteLine("Please enter a number.\n1 = area\n2 = circumfrence");
+          x = Convert.ToInt32(Console.ReadLine());
+          switch (x)
+          {
+              case 1:
+              result = side1 * side2;
+              Console.WriteLine("The area of the square is " + result + "cm2!");
+              break;
+
+              case 2:
+              result2 = 2 * side1 + 2 * side2;
+              Console.WriteLine("The circumfrence of the square is " + result2 + "cm!");
+              break;
+
+              default: 
+              Console.WriteLine("Error! You didn't input a number from 1 - 2!");
+              break;
+          }
+          Console.WriteLine("Press any button to close the calculator.");
+          Console.ReadKey();
+
+        }
+    }
+}
